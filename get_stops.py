0210650096651e206b4cf139e4b75ba249bee73c
richardmatusch <richardmatusch23@gmail.com>
2024-11-04 23:32:07 +0100

    getting links...

--- get_stops.py
+++ get_stops.py
@@ -7,4 +7,13 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 options = soup.find_all('option')
 
-print(options)
+stops = []
+
+for option in options:
+    n = option.get('value')
+    if n:
+        stop = [f"https://www.dpmk.sk/cp/zastavka/{n[5:]}", option.get_text().strip()]
+        stops.append(stop)
+
+for stop in stops:
+    print(stop)
