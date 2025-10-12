8e6df3ba36313fbc44f14e12cc1d8be0b1d29af8
richardmatusch <richardmatusch23@gmail.com>
2025-10-12 14:45:34 +0200

    added direction and line number

--- get_times.py
+++ get_times.py
@@ -15,7 +15,6 @@ def get_type_of_day():
     type_of_day = requests.get(url_for_type_of_day).text
     determined_class = type_of_day + " table-wrapper time"
     
-    print(f"today's timetable is for: {type_of_day}")
     return determined_class
 
 
@@ -26,24 +25,42 @@ def get_times_for_today(url, determined_class):
     soup = BeautifulSoup(response.content, 'html.parser')
     
     day_today = soup.find('div', class_=determined_class)
-    time_spans = day_today.find_all('span', attrs={'data-time': True}) #type: ignore
+    time_spans = day_today.find_all('span', attrs={'data-time': True})
     data_times = [span['data-time'] for span in time_spans]
     
     return data_times
     
     
 def get_line_number(url):
-    # get tram/bus line number
     
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    line_number = soup.find('div', class_='line')
-    print(line_number)
+    line_number = soup.find('h1').find('span').get_text()
+  
+    return line_number
     
     
-type_of_day_today = get_type_of_day()    
-todays_times = get_times_for_today(old_hospital_lines[0], type_of_day_today)
+def get_line_direction(url):
+    
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    line_direction = soup.find('strong', class_='to').get_text()
+    
+    return line_direction
+    
 
+lines_data =  {}
 
-print(todays_times)
+type_of_day_today = get_type_of_day()   
+
+first_line = old_hospital_lines[0]
+
+line_number = get_line_number(first_line)
+line_direction = get_line_direction(first_line)
+line_times = get_times_for_today(first_line, type_of_day_today)
+
+print(line_number)
+print(line_direction)
+print(line_times)
