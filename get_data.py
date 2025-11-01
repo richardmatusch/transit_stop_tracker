bf5737da901520bb3eac56f860d1309e58753de2
richardmatusch <richardmatusch23@gmail.com>
2025-11-01 22:36:35 +0100

    refactor continues

--- get_data.py
+++ get_data.py
@@ -31,12 +31,76 @@ def get_type_of_day():
     determined_class = type_of_day + " table-wrapper time"
     
     return determined_class
+    
+def get_times_for_today(url, determined_class):
+    # Get times based on type of day (working-days, holidays, free-days)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    day_today = soup.find('div', class_=determined_class)
+    if not day_today:
+        return []
+
+    time_spans = day_today.find_all('span', attrs={'data-time': True})
+    data_times = [convert_data_time_to_time(span['data-time']) for span in time_spans if 'data-time' in span.attrs]
+
+    return data_times
+
+def get_line_number(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    h1 = soup.find('h1')
+    if not h1:
+        return None
+
+    span = h1.find('span')
+    if not span:
+        return None
+
+    return span.get_text()
+
+def get_line_direction(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    strong = soup.find('strong', class_='to')
+    if not strong:
+        return None
+
+    return strong.get_text()
+    
+def convert_data_time_to_time(time_str):
+    time_str = time_str.zfill(4)
+    hours = time_str[:2]
+    minutes = time_str[2:]
+    
+    return f"{hours}:{minutes}"
 
 
 with open("config.json", "r", encoding="utf-8") as f:
     config = json.load(f)
 
 stop_link = config["tracked_stop"]["link"]
+stop_name = config["tracked_stop"]["name"]
 
 stop_lines = get_lines_from_stop(stop_link)
-print(stop_lines)
+
+type_of_day_today = get_type_of_day()   
+
+data = {
+    "stop_name": stop_name,
+    "stop_link": stop_link,
+    "lines_and_directions": { 
+    }
+}
+
+for line in stop_lines:
+    line_and_direction = f'{get_line_number(line)} -> {get_line_direction(line)}'
+    
+    if line_and_direction not in data['lines_and_directions']:
+        data['lines_and_directions'][line_and_direction] = {}
+        data['lines_and_directions'][line_and_direction]['Times'] = get_times_for_today(line, type_of_day_today)
+    
+with open('data.json', 'w', encoding='utf-8') as json_file:
+    json.dump(data, json_file, indent=4, ensure_ascii=False)
